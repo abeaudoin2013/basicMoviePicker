44963442e850819ba5f0961c8b7ad6bd725d7ff7
Andy <abeaudoin2013@gmail.com>
2015-12-11 10:46:19 -0500

    fixed routing issue

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,18 +1,21 @@
 class QuizzesController < ApplicationController
-	before_action :set_quiz, only: :show
+	# before_action :set_quiz, only: :show
 
   def index
   	@quizzes = Quiz.all
   end
 
   def show
+
+    @quiz = Quiz.find(params[:id])
+
   end
 
-  private
+  # private
 
-  def set_quiz
-  	@quiz = Quiz.find_by(params[:id])
-    current_quiz = params[:id]
-  end
+  # def set_quiz
+  	
+  #   # current_quiz = params[:id]
+  # end
 
 end
